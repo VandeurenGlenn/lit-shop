957edc60b0b0af19081094d9f3e34913b80495e1
Glenn Vandeuren (aka Iondependent) <vandeurenglenn@gmail.com>
2025-01-20 20:31:15 +0100

    server: images, add option to use firebaseKey

--- packages/server/src/routes/images/routes.images.ts
+++ packages/server/src/routes/images/routes.images.ts
@@ -1,3 +1,4 @@
+import { getDatabase } from 'firebase-admin/database'
 import type { Context } from 'koa'
 import Router from 'koa-router'
 import mime from 'mime-types'
@@ -5,8 +6,31 @@ import request from 'request'
 
 const router = new Router()
 
+const database = getDatabase()
+
+const imagesRef = database.ref('images')
+const images = {}
+
+imagesRef.on('child_added', (snapshot) => {
+  images[snapshot.key] = snapshot.val()
+})
+
+imagesRef.on('child_changed', (snapshot) => {
+  images[snapshot.key] = snapshot.val()
+})
+
+imagesRef.on('child_removed', (snapshot) => {
+  delete images[snapshot.key]
+})
+
 router.get('/api/image', async (ctx: Context) => {
-  const imageUrl = ctx.request.query.image as string
+  const firebaseKey = ctx.request.query.firebaseKey as string
+  let imageUrl: string
+
+  if (firebaseKey) imageUrl = images[firebaseKey]?.link
+
+  if (!imageUrl) imageUrl = ctx.request.query.image as string
+
   if (imageUrl) {
     ctx.body = request(imageUrl)
     const type = mime.lookup(imageUrl)
