0711ef0e9448a1008774edc610b1c50f65a96b2e
Glenn Vandeuren (aka Iondependent) <vandeurenglenn@gmail.com>
2025-03-01 17:47:32 +0100

    server: add test config

--- packages/server/src/routes/payconiq/constants.ts
+++ packages/server/src/routes/payconiq/constants.ts
@@ -2,5 +2,10 @@ const isProduction = process.env.NODE_ENV === 'production'
 
 export const CREATE_PAYMENT = '/checkout/payconiq/createPayment'
 export const CANCEL_PAYMENT = '/checkout/payconiq/cancelPayment'
-export const CALLBACK_URL = 'https://api.hellonewme.be/checkout/payconiq/callbackUrl'
-export const API_URL = `https://${!isProduction ? 'ext.' : ''}api.payconiq.com/v3/payments`
+export const CALLBACK_URL = isProduction
+  ? 'https://api.hellonewme.be/checkout/payconiq/callbackUrl'
+  : 'http://localhost:3005/checkout/payconiq/callbackUrl'
+
+export const API_URL = isProduction
+  ? 'https://api.payconiq.com/v3/payments'
+  : 'http://localhost:9090/payconiq/v3/payments'
